317a869d3d3386f11431d5214fa7d3c836d575c2
jorge mario mora <jorgemariomoramolina@gmail.com>
2022-07-22 15:12:53 -0500

    clase 06: se termina los nombre de los generos de los film

--- src/main.js
+++ src/main.js
@@ -32,3 +32,36 @@ async function trendingPreview() {
 };
 
 trendingPreview();
+
+
+async function categoriesPreview(){
+
+    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+    const data = await res.json();
+
+    const categories = data.genres;
+    console.log(categories);
+
+    categories.forEach(genre => {
+
+        const previewCategoriesList = document.querySelector('#categoriesPreview  .categoriesPreview-list');
+
+        const categoriesContainer = document.createElement('div');
+        categoriesContainer.classList.add('category-container');
+
+        const categoriesTitle = document.createElement('h3');
+        categoriesTitle.classList.add('category-title');
+        categoriesTitle.setAttribute('id','id' + genre.id);
+        const categoryName = document.createTextNode(genre.name);
+
+        previewCategoriesList.appendChild(categoriesContainer);
+        categoriesContainer.appendChild(categoriesTitle);
+        categoriesTitle.appendChild(categoryName)
+
+
+        
+    });
+
+}
+
+categoriesPreview();
